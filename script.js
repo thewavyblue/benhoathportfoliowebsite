2d3a689384ea152434a4d41acd28626424dda12d
Ben Hoath <thewavyblue@gmail.com>
2024-01-30 21:58:21 +0000

    v.0.13
    Added 'back to portfolio' links at the top and bottom of every project inpage
    
    Added a 'back to top' button with functionality to every page.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const btnMenuMob = document.getElementById('nav-menu-mob');
-
     const btnDev = document.getElementById('btn_dev');
     const btnDesign = document.getElementById('btn_design');
     const btnAll = document.getElementById('btn_all');
     let projectCards = document.querySelectorAll('.project-card');
-    
-    let btnNav = document.querySelectorAll('main-nav.li');
-
 
     btnDesign.addEventListener('click', function() {
         // Show only 'design' projects, hide others
@@ -37,8 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
         projectCards.forEach(function(card) {
             card.style.display = 'flex';
         });
-    }); 
-
+    });
+    
 });
 
 let navMenuListMob = document.querySelector('.nav-menu-list-mob');
@@ -49,4 +44,26 @@ function menu() {
     } else {
         navMenuListMob.style.display = 'none';
     }
+}
+
+// Scroll to top button
+
+// Get the button
+let btnBackToTop = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        btnBackToTop.style.display = "block";
+    } else {
+        btnBackToTop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 }
