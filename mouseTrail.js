47ac94d0a52f5f2c7a5bca4828294577b2f84026
Ben <thewavyblue@gmail.com>
2024-03-12 20:51:07 +0000

    v1.6.4 Hero functionality – Fixed!
    I've fixed several things about the text message bubble pointer follower, including speech bubble tick placement and 'close' function on dblClick event incase its annoying for the user.
    
    Tested CSS across all screen sizes. Turned off for mobile because it doesn't translate well (you need a larger screen size to make the most of it really!)

--- mouseTrail.js
+++ mouseTrail.js
@@ -3,23 +3,28 @@ import { quotes } from "./quotes.js";
 
 // declare DOM variables to get 'statement' div and its child
 const statement = document.querySelector(".statement");
-const statementQuote = document.querySelector(".statement-quote");
+const statementQuote = document.querySelector(".statement-quote"); 
 
 // declare the quoteIndex which determines the quote displayed, and the last known position of the statement bubble.
 let quoteIndex = 0;
 let last = { x: 0, y: 0 };
 
+function closeStatementBubble() {
+    statement.classList.add("hidden");
+}
+
 const activateBubble = (bubble, x, y) => {
     bubble.style.left = `${x}px`;
     bubble.style.top = `${y}px`;
     last = { x, y };
+    statement.addEventListener("dblclick", closeStatementBubble);
 }
 
 const calculateDistance = (x, y) => Math.hypot(x - last.x, y - last.y);
 
 const getStatement = (index) => {
     const adjustedIndex = index % quotes.length;
-    statementQuote.innerHTML = `I'm ${quotes[adjustedIndex].quote}`;
+    statementQuote.innerHTML = `I'm ${quotes[adjustedIndex].quote}<br><span class="fine-text">[Double click / tap anywhere to hide]</span>`;
 }
 
 const handleMouseMove = (e) => {
