d9a38674ed2467e11d39519e0dfeb9c96cd178b1
Ben <thewavyblue@gmail.com>
2024-03-25 19:12:36 +0000

    v1.9 Installed HotJar to understand user behaviour

---
+++ .gitignore/loadProjects.js
@@ -0,0 +1,32 @@
+import { project } from "./projects";
+
+<article class="col project-card tag-style-1 design dev">
+<div class="col-md-4 project-content-left">
+        <ul class="category-tags pad-b-2">
+            <li class="tag tag-style-5">Design</li>                         
+            <li class="tag tag-style-6">Development</li>
+        </ul>
+        <a href="/project-ce-cpt.html" rel="noreferrer noopener"><h3>Creative Estuary – Cultural Planning Toolkit</h3></a>
+        <p>Developing cultural infrastructure requires a steady focused effort across long periods of time. The Cultural Planning Toolkit will help users navigate the deliverability of cultural infrastructure</p>
+        <ul class="category-tags pad-b-2">
+            <li class="tag tag-style-2">Webflow</li>
+            <li class="tag tag-style-3">UX</li>
+            <li class="tag tag-style-4">UI</li>
+            <li class="tag tag-style-5">Branding</li>
+        </ul>
+        <div class="external-links">
+            <a href="/project-ce-cpt.html" rel="noreferrer noopener" class="sm-lockup">
+                <img loading="lazy" src="/imgs/svg/icon-read-more.svg" alt="Read more">
+                <p class="keyword">Read more</p>
+            </a>
+            <a href="https://www.creativeestuary.com/culturalplanningtoolkit" rel="noreferrer noopener" target="_blank" class="sm-lockup">
+                <img loading="lazy" src="/imgs/svg/external-link.svg" alt="Go to live site">
+                <p class="keyword">Go to live site</p>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="col project-content-right">
+    <a href="/project-ce-cpt.html" rel="noreferrer noopener" tabindex="-1"><img loading="lazy" class="thumb-img" src="/imgs/creative-estuary/Creative-Estuary-CPT-Thumb02.png" alt="Creative Estuary – Cultural Planning Toolkit"></a>
+</div>
+</article>
