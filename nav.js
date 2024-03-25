f97095a79d36ef32753bd695fb721a78c1ec00c8
Ben <thewavyblue@gmail.com>
2024-03-25 17:14:08 +0000

    v1.7 Nav bar hide on scroll (mob only)
    Added this small functionality to increase screen real estate for mobile users.

--- nav.js
+++ nav.js
@@ -1,26 +1,35 @@
-let navMenuListMob = document.querySelector('.nav-menu-list-mob');
-const menuItems = document.querySelectorAll('.menu-link');
+let navMenuListMob = document.querySelector(".nav-menu-list-mob");
+const menuItems = document.querySelectorAll(".menu-link");
 
 function menu() {
-    if (navMenuListMob.style.display === 'none' ) {
-        navMenuListMob.style.display = 'flex';
+    if (navMenuListMob.style.display === "none" ) {
+        navMenuListMob.style.display = "flex";
     } else {
-        navMenuListMob.style.display = 'none';
+        navMenuListMob.style.display = "none";
     }
 }
 
 menuItems.forEach(function(item) {
-    item.addEventListener('click', function() {
+    item.addEventListener("click", function() {
         // Close the menu
-        navMenuListMob.style.display = 'none';
-        console.log('menu closed');
+        navMenuListMob.style.display = "none";
+        console.log("menu closed");
     });
 });
 
-
-
-// Hide the nav bar on scroll
-// document.addEventListener('scroll', function() {
-//     let navBarMob = document.querySelector('.nav-bar');
-//     navBarMob.classList.add = 'nav-bar-scrolled';
-// });
+// On mobile, when the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
+const mobScreenSize = 425;
+const screenWidth = window.innerWidth;
+let prevScrollPos = window.scrollY;
+window.onscroll = () => {
+    let currentScrollPos = window.scrollY;
+    if (screenWidth <= mobScreenSize) {
+        if (prevScrollPos > currentScrollPos) {
+            document.getElementById("nav-bar").style.top = "0";
+        } else {
+            document.getElementById("nav-bar").style.top = "-6rem";
+        }
+        console.log(screenWidth);
+        prevScrollPos = currentScrollPos;
+    } 
+}
